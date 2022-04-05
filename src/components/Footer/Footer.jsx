e836d39fbf681bc10fedb27aae6bdf84a1794149
usama885 <engusamaasif885@gmail.com>
2022-04-06 00:32:06 +0500

    commit from subhan

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import footer_logo from '../Images/footer-logo.jpg'
-// var date = new Date();
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -17,18 +16,15 @@ const Footer = () => {
           <div className='col-11 mt-4 '>
             <div className='row d-flex justify-content-around'>
               <div className='col-12 col-md-4 justify'>
-                {/* <img className='img-fluid mt-4' src={footer_logo} width={200} alt="Footer_Logo" /> */}
                 <h1 style={{color: 'white'}}>Soft Kloud</h1>
                 <p className='mt-4'>We Provide ourselves in our ability to deliver our client’s vision. Customer satisfaction is our number one priority. We understand that our work is our expression to the outside world and we make sure that we produce high-quality solutions.</p>
               </div>
               <div className='col-12 col-md-2 justify' style={{color: 'white'}}>
                 <h2 className='mb-4 mt-4' >Quick Links</h2>
-                <h5>
-                <Link className="nav-link" style={{color: 'white'}} to="/">Home</Link>
-                </h5>
-                <h5>About</h5>
-                <h5>Services</h5>
-                <h5>Contact</h5>
+                <h5><Link className="nav-link p-0" style={{color: 'white'}} to="/">Home</Link></h5>
+                <h5><Link className="nav-link p-0" style={{color: 'white'}} to="About">About</Link></h5>
+                <h5><Link className="nav-link p-0" style={{color: 'white'}} to="Services">Services</Link></h5>
+                <h5><Link className="nav-link p-0" style={{color: 'white'}} to="Contact">Contact</Link></h5>
               </div>
               <div className='col-12 col-md-2 justify'>
                 <h2 className='mb-4 mt-4'>Get In Touch</h2>
